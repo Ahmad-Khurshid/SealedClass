3e35f84b1feda14a03b681bc38aeb089b02da9ca
Ahmad <ahmadking531049@gmail.com>
2023-08-02 19:25:17 +0500

    Add project files.

---
+++ SealedClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClass
+{
+    /*sealed class parent */
+    class parent 
+    {
+        public void show1()
+        {
+            Console.WriteLine(" method of parent class");
+
+        }
+    }
+    class child : parent
+    {
+        public void show2()
+        {
+            Console.WriteLine(" method of child class");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            parent obj1 = new parent();
+            obj1.show1();
+            child obj2 = new child();
+            obj2.show2();
+            obj2.show1 ();
+
+
+            Console.ReadLine();
+        }
+    }
+}
